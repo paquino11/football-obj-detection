5c8c82dab1f847a9c77611c3c7e736ad6b64a7f7
Pedro Aquino <pedro.aquino479@gmail.com>
2024-09-07 15:34:39 +0200

    improve model and stubs

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
     # initialize tracker
-    tracker = Tracker('models/best.pt')
+    tracker = Tracker('models/even_best.pt')
 
     tracks = tracker.get_object_tracks(video_frames, 
                                        read_from_stub=True, 
